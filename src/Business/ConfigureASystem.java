86448808bb152704c49d2c29ec9660254db532c1
BanishGanesh <114786051+BanishGanesh@users.noreply.github.com>
2022-12-07 22:46:10 -0500

    Update ConfigureASystem.java

--- src/Business/ConfigureASystem.java
+++ src/Business/ConfigureASystem.java
@@ -23,16 +23,6 @@ public class ConfigureASystem {
           Community c1 = new Community();
           c1.setName("Avenue St.");
           People p = new People("AAA-GG-SSSS","System","Admin","admin@neu.com","","","","","","",true,false,false,false,new LoginCredentials("admin","password"));
-<<<<<<< HEAD
-          People I = new People("AAA-GG-SSSS","Insurance","Admin","Iadmin@neu.com","","","","","","",true,false,false,false,new LoginCredentials("iadmin","password"));
-          PeopleDirectory peopDir = new PeopleDirectory();
-          p.setUserName("admin");
-          p.setPassword("password");
-          I.setUserName("iadmin");
-          I.setPassword("password");
-          peopDir.addPerson(p);
-          peopDir.addPerson(I);
-=======
           People h = new People("SSN1","Hospital","Admin","hospadmin@neu.com","","","","","","",false,false,true,false,new LoginCredentials("hospadmin","hosppassword"));
           PeopleDirectory peopDir = new PeopleDirectory();
           p.setUserName("admin");
@@ -41,7 +31,6 @@ public class ConfigureASystem {
           h.setPassword("hosppassword");
           peopDir.addPerson(p);
           peopDir.addPerson(h);
->>>>>>> dev
           sys.setPeopleDirectory(peopDir);
          sys.commDirectory.addCommunity(c);
          sys.commDirectory.addCommunity(c1);
